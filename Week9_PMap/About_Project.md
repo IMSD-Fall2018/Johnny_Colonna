9a8a61d7e6f3f3888d261170841cfef19ef20e8f
Student <student@ent-v103a-m015.local>
2018-11-05 13:22:02 -0500

    finished Projection Map Project

---
+++ Week9_PMap/About_Project.md
@@ -0,0 +1,9 @@
+# Projection Mapping Project
+
+Team:
+Oleg Ligay
+Bryan Soasti
+Johnny Colonna
+Kiara Wright
+
+We took a box and filled it with projection cubes. In order to contain cubes we put invisible walls and we put a black floor underneath our box. As for user input we used key presses to delete the cubes that fall out of the main box. Up arrow moves a "killzone" to collide with the cubes and destroys them upon impact. And, down arrow resets the "killzone". We also added sound to play on collision of the cubes.
---
+++ Week9_PMap/Assets/MoveKiller.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveKiller : MonoBehaviour {
+    public float movementSpeed = 1f;
+    Vector3 newPosition = Vector3.zero;
+    Vector3 returnPosition= Vector3.zero;
+	// Use this for initialization
+	void Start () {
+        newPosition = this.transform.position;
+        returnPosition = newPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition.y += (Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition.y = returnPosition.y;
+        }
+
+        this.transform.position = newPosition;
+	}
+}
